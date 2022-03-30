6c0df7eb1128d0124120a708af489aec7afa93c7
nehabht <ne.bhatti2@icloud.com>
2022-03-30 10:29:21 +0200

    add correction snack 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,9 +10,9 @@ const second_number = prompt("inserisci il secondo numero")
 console.log(first_number)
 console.log(second_number)
 
-if (first_number >= second_number){
+if (first_number > second_number){
     console.log(`il primo vince: ${first_number}`);
-}else if (first_number <= second_number){
+}else if (first_number < second_number){
     console.log(`il secondo vince: ${second_number}`);
 }else {
     console.log("pareggio riprova!")
