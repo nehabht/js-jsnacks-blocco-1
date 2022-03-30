5f3e816d63ff6dada920d6acee20cc6c1a68e0b1
nehabht <ne.bhatti2@icloud.com>
2022-03-30 10:27:16 +0200

    add snack 1

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+// SNACK 1 
+// L’utente inserisce due numeri in successione, con due prompt.
+// Il software stampa il maggiore.
+
+
+const first_number = prompt("inserisci il primo numero")
+
+const second_number = prompt("inserisci il secondo numero")
+
+console.log(first_number)
+console.log(second_number)
+
+if (first_number >= second_number){
+    console.log(`il primo vince: ${first_number}`);
+}else if (first_number <= second_number){
+    console.log(`il secondo vince: ${second_number}`);
+}else {
+    console.log("pareggio riprova!")
+}
