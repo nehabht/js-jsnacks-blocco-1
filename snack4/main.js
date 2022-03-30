52d6119bf275b3933841dbaecb19fee1cc221520
nehabht <ne.bhatti2@icloud.com>
2022-03-30 12:25:08 +0200

    add snack 4

---
+++ snack4/main.js
@@ -0,0 +1,27 @@
+
+// // SNACK 4
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, 
+// chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+
+const ospiti = prompt("nome?")
+
+const invitati = ["gatsby", "mark", "eleonora", "sun"]
+
+let puo_entrare;
+
+for (let i = 0; i < invitati.length; i++){
+
+    if (invitati[i] === ospiti){
+        puo_entrare = true
+        
+    }
+    
+}
+
+
+if (puo_entrare){
+    console.log("entra");
+} else {
+    console.log("non entra")
+}
